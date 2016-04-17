a1f5de881b270e08fa1c1cd525460dd2dcbc3459
PressW <gullysquad02@gmail.com>
2016-04-16 20:54:14 -0700

    Added write to file functionality
    Revision removed aggregation of all algorithms into single file. Running four files instead

--- project1/algorithm4.c
+++ project1/algorithm4.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <sys/time.h>
 
+typedef struct {
+	int start;
+	int end;
+	int value;
+} tTuple;
+
 struct timeval time_start, time_stop, run_time;
 
 
@@ -12,11 +18,12 @@ struct timeval time_start, time_stop, run_time;
 *************************************************************** */
 
 
-int max_sum_subArray_4(int* array);
+tTuple max_sum_subArray_4(int* array);
 int* parseArray(char* line);
-void getLine(FILE* file);
+void getLine(FILE* inFile, FILE* outFile);
 FILE* openFile(const char* filname);
-void timeRightNow();
+void timeRightNow(FILE* outFile);
+int arraySize;
 
 
 
@@ -31,22 +38,15 @@ void timeRightNow();
 	const char* fileOut = "MSS_TestResults.txt";
 	FILE *fpIn, *fpOut;
 	
-	if (fpIn = openFile(fileIn)){
-		printf("\nSuccessfully return pointer to %s: %p\n", fileIn, fpIn);
-	}
+	fpIn = openFile(fileIn);
+	fpOut = openFile(fileOut);
 	
-	if (fpOut = openFile(fileOut)){
-		printf("\nSuccessfully return pointer to %s: %p\n", fileOut, fpOut);
-	}
-	
-	getLine(fpIn);
+	getLine(fpIn, fpOut);
 	
 	fclose(fpIn);
 	fclose(fpOut);
-	
-	
-   
-   return 0;
+	   
+	return 0;
 }
 
 
@@ -55,20 +55,32 @@ void timeRightNow();
 *						SUPPLEMENTAL FUNCTION					 *
 *************************************************************** */
  
-
-int max_sum_subArray_4(int* array){
-	
-	int size = sizeof(array);
-	int min = 32767;
-	
-	int temp_max, current_max, combo = 0;
  
-	for (int i = 0; i < size; i++){
+tTuple max_sum_subArray_4(int* array){
+	
+	int min = 32767;
+	tTuple result;
+	result.value, result.start = 0;
+	result.end = arraySize;
+	
+	int temp_max, current_max = 0;
+ 
+	for (int i = 0; i < arraySize; i++){
 		min = min < array[i] ? min : array[i];
-		current_max = array[i] > (current_max + array[i]) ? array[i] : (current_max + array[i]);
-		temp_max = temp_max > current_max ? temp_max : current_max;
+		if (array[i] > (current_max + array[i])){
+			result.start = i;
+			current_max = array[i];
+		}
+		else{
+			current_max = current_max + array[i];
+		}
+
+		if (result.value <= current_max){
+			result.value = current_max;
+			result.end = i;
+		}
 	}
-	return temp_max;
+	return result;
 }
 
 
@@ -79,6 +91,7 @@ int* parseArray(char* passedLine){
 	int nextNum, i = 0;
 	int* parsedArray = malloc(256);
 	
+	memset(&line, 0, sizeof(line));
 	memcpy(&line, passedLine, 256);
 	line[0] = ' ';
 	token = strtok(line, ",");
@@ -90,27 +103,29 @@ int* parseArray(char* passedLine){
 		i++;
 		token = strtok(NULL,",");
 	}
+	arraySize = i;
 	return parsedArray;
 }
 
 
-void getLine(FILE* file){
-//void getLine(FILE* inFile, FILE* outFile){
+void getLine(FILE* inFile, FILE* outFile){
 	
 	char lineIn[256];
 	int* parsedArray, max;
-	while ( fgets(lineIn, sizeof(lineIn), file) != NULL ){
+	tTuple subArray;
+	while ( fgets(lineIn, sizeof(lineIn), inFile) != NULL ){
 		
+		printf("\n*********** ALGORITHM 4 ***********\n");
+		fprintf(outFile, "\n*********** ALGORITHM 4 ***********\n");
 		parsedArray = parseArray(lineIn);
 		gettimeofday(&time_start, NULL);
-		max = max_sum_subArray_4(parsedArray);
-		gettimeofday(&time_stop, NULL);
-		
-		printf("\nThe max of the array is: %d", max);
-		timeRightNow();
+		subArray = max_sum_subArray_4(parsedArray);
+		gettimeofday(&time_stop, NULL);		
+		printf("\nThe max sub array is: %d\nStart: %d\nEnd: %d", subArray.value, subArray.start, subArray.end);
+		fprintf(outFile, "\nThe max sub array is: %d\nStart: %d\nEnd: %d", subArray.value, subArray.start, subArray.end);
+		timeRightNow(outFile);
 	}
 	free(parsedArray);
-	
 }
 
 
@@ -127,11 +142,11 @@ FILE* openFile(const char* filename){
 }
 
 
-void timeRightNow(){
+void timeRightNow(FILE* outFile){
 		
 	timersub(&time_stop, &time_start, &run_time);
 	printf("\nComputed in %ld.%06ld\n", (long int)run_time.tv_sec, (long int)run_time.tv_usec);
-
+	fprintf(outFile, "\nComputed in %ld.%06ld\n", (long int)run_time.tv_sec, (long int)run_time.tv_usec);
 }
 
 
